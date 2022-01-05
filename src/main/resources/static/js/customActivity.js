67da388072b347dd2506993d86fdc70152c856ab
Tabreze <career.karimee@gmail.com>
2022-01-05 16:01:35 +0530

    API calls

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -141,7 +141,7 @@ define([
         }
 
 
-fetch('https://mc-260crls51zy9yd64d27td22t8.auth.marketingcloudapis.com/v1/requestToken', 
+fetch('https://mc-260crls51zy9yd64d27td22t8.auth.marketingcloudapis.com/v2/token', 
 	{
 	//mode: "opaque",	  
 	 method: "POST",
